c7b499250ab4641e4c9fedae84121c0454fcf057
Matt Bryan <mpbryan90@gmail.com>
2015-11-16 17:56:42 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var swig = require('swig');
+var routes = require('./routes/');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({cache: false});
+
+app.use(morgan('dev'));
+// app.use(function(request, response, next){
+// 	console.log(request.method + " " + request.url + " " + response.statusCode);
+// 	next();
+// });
+
+app.use('/', routes);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next){
+	var err = new Error('could not find route');
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next){
+	res.status(err.status || 500).send('ERROR: ' + err.message);
+});
+
+app.listen(3000, function(){
+	console.log("server listening...");
+});
