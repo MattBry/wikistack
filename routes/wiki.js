df32466a690cc9cd066ec6d0da3b8624778a17e3
Matt Bryan <mpbryan90@gmail.com>
2015-11-17 10:58:01 -0500

    added form inputs and set up POST route for submission

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../models/');
+var Page = models.Page;
+var User = models.User;
 // router.get('/', function(req, res){
 //     res.render('index');
 // });
@@ -11,7 +13,14 @@ router.get('/', function(req,res){
 });
 
 router.post('/', function(req,res){
-    res.send("Submitting a new wiki page");
+    var page = new Page({
+    	title: req.body.title,
+    	content: req.body.content,
+    	urlTitle: urlTitleMaker(req.body.title)
+    })
+    page.save().then(function(){
+    res.redirect('/');
+});
 });
 
 
@@ -20,5 +29,7 @@ router.get('/add', function(req,res){
     res.render('addpage');
 });
 
-
+function urlTitleMaker(title) {
+	return title.replace(/\s/g, "_").replace(/\W/g, "");
+}
 module.exports = router;
